6b2f42eb05ab7868984143ac2980b69fb67f9c10
hfcaio <hfcaio@gmail.com>
2023-06-03 21:59:20 -0300

    :sparkles: programa para detecção do verde na tela

--- video.py
+++ video.py
@@ -1,5 +1,8 @@
 import cv2
 from Funcao1 import *
+from warning_green import *
+
+atual = -1; past = -1
 
 if __name__ == "__main__":
     camera = cv2.VideoCapture(0)
@@ -8,9 +11,14 @@ if __name__ == "__main__":
     while validation:
         validation, frame = camera.read()
         resultado, mascara = mask_apply(frame)
+        atual = warning_green(mascara)
         cv2.imshow("Cubos hsv", resultado)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        if past != atual and atual:
+            print("apareceu verde") 
+        past = atual
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
